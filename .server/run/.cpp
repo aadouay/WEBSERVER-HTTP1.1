f32226cb42d7a27eb35eb12aa58904308cf85fbc
abdallahelmadi <abdallahelmadi42@gmail.com>
2025-12-19 17:00:40 +0100

    finish request container

--- .server/run/.cpp
+++ .server/run/.cpp
@@ -11,73 +11,21 @@ std::string getNetworkIP();
 int run(long long start) {
   for (std::size_t i = 0; i < server.length(); i++) {
 
-    // create socket
-    int serverFD = socket(AF_INET, SOCK_STREAM, 0);
-    if (serverFD < 0) {
-      console.issue("Failed to create socket for " + server[i].name());
-      continue;
+    (void)start;
+
+    request req = request("GET /hello#?test=b HTTP/1.1\r\n\r\ns");
+
+    std::cout << "Method: " << req.getMethod() << std::endl;
+    std::cout << "Path: " << req.getPath() << std::endl;
+    std::cout << "HTTP: " << req.getHTTP() << std::endl;
+    std::map<std::string, std::string> headers = req.getHeaders();
+    for (std::map<std::string, std::string>::const_iterator it = headers.begin(); it != headers.end(); ++it) {
+      std::cout << "Header: " << it->first << " => " << it->second << std::endl;
     }
+    std::cout << "Body: " << req.getBody() << std::endl;
 
-    // set socket options
-    int opt = 1;
-    if (setsockopt(serverFD, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0) {
-      console.issue("Failed to set socket options for " + server[i].name());
-      close(serverFD);
-      continue;
-    }
-
-    // Bind socket
-    struct sockaddr_in address;
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY; // bind to all interfaces
-    // (for specific IP, add `ntohs(INADDR_LOOPBACK)` or use `inet_addr("x.x.x.x")`)
-    address.sin_port = htons(server[i].port()); // bind to specified port
-
-    if (bind(serverFD, (struct sockaddr*)&address, sizeof(address)) < 0) {
-      console.issue("Failed to bind socket for " + server[i].name());
-      close(serverFD);
-      continue;
-    }
-
-    // Listen for connections
-    if (listen(serverFD, 10) < 0) {
-      console.issue("Failed to listen on " + server[i].name());
-      close(serverFD);
-      continue;
-    }
-
-    console.init(server[i].port(), getNetworkIP(), server[i].name(), server[i].version());
-    // console.success("Ready in " + (num2str(time::calc(start, time::clock()))) + "ms");
-
-    // Accept and handle requests
-    while (true) {
-      struct sockaddr_in client_address;
-      socklen_t client_len = sizeof(client_address);
-      int client_fd = accept(serverFD, (struct sockaddr*)&client_address, &client_len);
-
-      if (client_fd < 0) {
-        continue;
-      }
-
-      // Read request (simple read)
-      char buffer[4096] = {0};
-      read(client_fd, buffer, sizeof(buffer) - 1);
-
-      (void)start; // suppress unused variable warning
-      std::cout << "Received request:\n" << buffer << std::endl;
-
-      // Send HTTP response
-      const char* response = 
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Type: text/plain\r\n"
-        "Content-Typpe: application/json\r\n" 
-        "Content-Length: 5\r\n"
-        "\r\n"
-        "hello";
-
-      send(client_fd, response, strlen(response), 0);
-      close(client_fd);
-    }
+    if (req.getBadRequest())
+      console.METHODS(req.getMethod(), req.getPath(), req.getBadRequest(), 1);
   }
-  return 1;
+  return 0;
 }
