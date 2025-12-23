0b5291dad4797af4dc7c612b8c6fd29c7a5c9d24
abdallahelmadi <abdallahelmadi42@gmail.com>
2025-12-23 11:36:52 +0100

    send request and print log on response class after parse data

--- .server/lib/response.hpp
+++ .server/lib/response.hpp
@@ -3,18 +3,32 @@
 #include <sstream>
 #include <map>
 #include <status.hpp>
+#include <console.hpp>
+#include <request.hpp>
+#include <time.hpp>
+#include <sys/socket.h>
 
 class response {
   private:
+    unsigned int _client;
+    long long _startRequestTime;
     unsigned int _code;
     std::map<std::string, std::string> _headers;
     std::string _body;
+    request _request;
 
   public:
-    response(unsigned int code, std::map<std::string, std::string> headers, std::string body)
-    : _code(code), _headers(headers), _body(body) {}
+    response(
+      unsigned int client,
+      long long startRequestTime,
+      unsigned int code,
+      std::map<std::string, std::string> headers,
+      std::string body,
+      request request
+    )
+    : _client(client), _startRequestTime(startRequestTime), _code(code), _headers(headers), _body(body), _request(request) {}
 
-    void send(void) {
+    void sendResponse(void) {
       std::stringstream response;
       response << "HTTP/1.1 " << this->_code << " " << status(this->_code).message() << "\r\n";
 
@@ -24,5 +38,8 @@ class response {
 
       response << "\r\n";
       response << this->_body;
+
+      send(this->_client, response.str().c_str(), response.str().length(), 0);
+      console.METHODS(this->_request.getMethod(), this->_request.getPath(), this->_code, time::calcl(this->_startRequestTime, time::clock()));
     }
 };
