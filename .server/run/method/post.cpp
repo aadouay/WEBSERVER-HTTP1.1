f7c833bfa8839948bec63fc04c5a54ded9045da2
Batrii <dhj.nafiai@gmail.com>
2025-12-26 18:34:50 +0100

    Post handle but needs some specifcation

--- .server/run/method/post.cpp
+++ .server/run/method/post.cpp
@@ -5,6 +5,7 @@
 #include <time.hpp>
 #include <sys/socket.h>
 #include <response.hpp>
+#include <fstream>
 class urlencoder {
   private:
     std::map<std::string, std::string> _fields;
@@ -48,6 +49,70 @@ void urlencoder::parseBodyContent(std::string& content)
   }
 }
 
+void handle_multipart(const std::string& content, request& req) {
+  std::string boundaryname = req.getHeaders().at("Content-Type");
+  std::size_t boundary_pos = boundaryname.find("boundary=");
+  if (boundary_pos != std::string::npos){
+    std::string boundary = "--" + boundaryname.substr(boundary_pos + 9);
+    std::string boundary_break = boundary + "--";
+    std::size_t i = 0;
+    while (i < content.length()) {
+      std::size_t boundary_start = content.find(boundary, i);
+      std::size_t boundary_break_pos = content.find(boundary_break, i);
+      if (boundary_break_pos != std::string::npos) {
+        break;
+      }
+      if (boundary_start != std::string::npos){
+        std::size_t boundary_end = content.find(boundary, boundary_start + boundary.length());
+        if (boundary_end != std::string::npos) {
+          std::string part = content.substr(boundary_start + boundary.length() + 2, boundary_end - (boundary_start + boundary.length() + 2));
+          if (part.find("Content-Disposition: form-data;") != std::string::npos)
+          {
+            std::size_t filename_pos = part.find("filename=");
+            if (filename_pos != std::string::npos) {
+              std::string filename = part.substr(filename_pos + 10, part.find("\r\n", filename_pos) - (filename_pos + 10) - 1);
+              std::size_t data_start = part.find("\r\n\r\n");
+              if (data_start != std::string::npos) {
+                std::string filedata = part.substr(data_start + 4, part.length() - (data_start + 4) - 2);
+                std::string filepath = "./uploads/" + filename;
+                std::ofstream outfile(filepath.c_str());
+                outfile << filedata;
+                outfile.close();
+              }
+            }
+            else {
+              // Handle regular form field if necessary
+              std::size_t name_pos = part.find("name=");
+              if (name_pos != std::string::npos) {
+                std::string name = part.substr(name_pos + 6, part.find("\r\n", name_pos) - (name_pos + 6) - 1);
+                std::size_t data_start = part.find("\r\n\r\n");
+                if (data_start != std::string::npos) {
+                  std::string fielddata = part.substr(data_start + 4, part.length() - (data_start + 4) - 2);
+                  std::string stored_data = "Field Name: " + name + ", Value: " + fielddata + "\n";
+                  std::ofstream outfile("./uploads/form_fields.txt");
+                  outfile << stored_data;
+                  outfile.close();
+                }
+              }
+            }
+          }
+        }
+        i = boundary_end;
+      }
+      else {
+        break;
+      }
+    }
+  }
+}
+
+void handle_json(const std::string& content) {
+  std::ofstream outfile("./uploads/data.json");
+  outfile << content;
+  outfile.close();
+}
+
+
 void methodPost(int client, request& req, ctr& currentServer, long long startRequestTime) {
   (void)currentServer;
   if (req.getBody().empty()) {
@@ -71,7 +136,14 @@ void methodPost(int client, request& req, ctr& currentServer, long long startReq
       return;
     }
     else if (contentType.find("multipart/form-data") != std::string::npos)
-    {}
+    {
+      std::string contentbody = req.getBody();
+      handle_multipart(contentbody, req);
+      std::map<std::string, std::string> headers;
+      std::string body = "Multipart data received and processed.";
+      response(client, startRequestTime, 200, headers, body, req).sendResponse();
+      return;
+    }
     else if (contentType.find("text/plain") != std::string::npos)
     {
       std::string contentstored = req.getBody();
@@ -81,7 +153,14 @@ void methodPost(int client, request& req, ctr& currentServer, long long startReq
       return;
     }
     else if (contentType.find("application/json") != std::string::npos)
-    {}
+    {
+      std::string content = req.getBody();
+      handle_json(content);
+      std::map<std::string, std::string> headers;
+      std::string body = "JSON Data received:\n" + content;
+      response(client, startRequestTime, 200, headers, body, req).sendResponse();
+      return;
+    }
     else {
       std::map<std::string, std::string> headers;
       std::string body = "";
@@ -92,8 +171,7 @@ void methodPost(int client, request& req, ctr& currentServer, long long startReq
   else {
     std::map<std::string, std::string> headers;
     std::string body = "";
-    response res(client, startRequestTime, 411, headers, body, req);
-    res.sendResponse();
+    response(client, startRequestTime, 411, headers, body, req).sendResponse();
     return;
   }
 }
